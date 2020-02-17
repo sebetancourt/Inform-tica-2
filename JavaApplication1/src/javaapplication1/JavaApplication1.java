66465d91d59b5823d7a6d38fe23b4081c074752c
sebetancourt <61156654+sebetancourt@users.noreply.github.com>
2020-02-17 11:11:12 -0500

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,13 @@
+package javaapplication1;
+
+public class JavaApplication1 
+{
+
+    public static void main(String[] args) 
+    {
+        FrmAdivina frmAdivina=new FrmAdivina();
+        //frmAdivina.show();
+        frmAdivina.setVisible(true);
+    }
+    
+}
---
+++ JavaApplication1/src/javaapplication1/fmrAdivina.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author sebetancourtac
+ */
+class fmrAdivina {
+
+    static void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
