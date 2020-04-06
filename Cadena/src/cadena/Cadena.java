4d034db6ac7517e5a9e07f6efa41d4202939623c
sebetancourt <61156654+sebetancourt@users.noreply.github.com>
2020-04-05 23:20:26 -0500

    Add files via upload

---
+++ Cadena/src/cadena/Cadena.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadena;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Cadena {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmCadena FrmCadena=new FrmCadena();
+        //frmAdivina.show();
+       FrmCadena.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
