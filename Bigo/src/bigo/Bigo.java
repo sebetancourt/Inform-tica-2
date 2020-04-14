ae39ecd98011835ac48b1a2f08e90bc91f9aeece
sebetancourt <61156654+sebetancourt@users.noreply.github.com>
2020-04-14 17:14:50 -0500

    Add files via upload

---
+++ Bigo/src/bigo/Bigo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigo;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Bigo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         FrmBusqueda FrmBusqueda=new FrmBusqueda();
+        //frmAdivina.show();
+       FrmBusqueda.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
